a073c9e82d705f300b35999023e584311d89eb8d
Frank Basham <frankabasham@gmail.com>
2022-10-30 18:29:02 -0400

    change findPage to be async

--- findPage.js
+++ findPage.js
@@ -1,11 +1,27 @@
 import fs from "fs";
 
-function findPage(search) {
-  let data = fs.readFileSync("pageData.json", "utf-8");
-  let pages = JSON.parse(data);
-  return pages
-    .filter((page) => new RegExp(search, "i").test(page.text))
-    .map((page) => page.href);
+// function findPage(search) {
+//   let data = fs.readFileSync("pageData.json", "utf-8");
+//   let pages = JSON.parse(data);
+//   return pages
+//     .filter((page) => new RegExp(search, "i").test(page.text))
+//     .map((page) => page.href);
+// }
+
+async function findPage(search) {
+  try {
+    let data = await fs.promises.readFile("pageData.json", "utf-8");
+    let pages = JSON.parse(data);
+    return pages
+      .filter((page) => new RegExp(search, "i").test(page.text))
+      .map((page) => page.href);
+  } catch (e) {
+    return;
+  }
 }
 
-console.log(findPage("agile"));
+async function f() {
+  console.log(await findPage("agile"));
+}
+
+f();
